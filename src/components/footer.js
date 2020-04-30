2ae2f7e6169186a9de2ddffbfbef17ca95e27d29
wildeink <misterjoy@fastmail.com>
2020-04-30 07:44:03 -0400

    fixed footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import "../components/styles/footer.css"
 export default function footer() {
     return (
         <div className="footerbody">
-            <p className="ftrtext">&copy; 2020 Christopher J. Joy 😎 |  &nbsp;Github &nbsp;</p>
+            <p className="ftrtext">&copy; 2020 Christopher J. Joy 😎 |  &nbsp;<a target="_blank" href="http://www.github.com/wildeink/">Github</a> &nbsp;</p>
         <p className="ftrtext">Built with Gatsby</p>
         </div>
     )
